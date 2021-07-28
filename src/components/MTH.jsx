c0ddb60a753be34d38260193b60d59fb378a8de4
VahuBali <67645126+VahuBali@users.noreply.github.com>
2021-07-28 18:37:32 -0400

    initial build

--- src/components/MTH.jsx
+++ src/components/MTH.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function MTH() {
+function MTH() {
     return (
         <div className="h-screen font-main overflow-hidden">
             <h1 className="text-center bg-clip-text text-transparent bg-gradient-to-r from-bluegrad2 to-greengrad lg:text-7xl text-5xl font-bold">Meet the Hosts</h1>
@@ -9,12 +9,12 @@ export default function MTH() {
                 <div className="flex flex-col md:flex-row justify-around items-center px-10 gap-10">
 
                     <div className={"block w-1/2 md:w-max"}>
-                        <img src={'../assets/vasu.png'} />
+                        <img alt={"vasu"} src={'https://i.imgur.com/Jn9w8va.png'} />
                         <p className={"text-white font-main text-center mt-5 text-2xl md:text-5xl md:mt-10 font-semibold"}>Vasu Bansal</p>
                     </div>
 
                     <div className={"block w-1/2 md:w-max"}>
-                        <img src={'../assets/chinmay.png'} />
+                        <img alt="chinmay" src={'https://i.imgur.com/R0R4wH0.png'} />
                         <p className={"text-white font-main text-center mt-5 text-2xl md:text-5xl md:mt-10 font-semibold"}>Chinmay Sule</p>
 
                     </div>
@@ -25,3 +25,5 @@ export default function MTH() {
         </div>
     )
 }
+
+export default MTH
