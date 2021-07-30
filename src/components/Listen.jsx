e1fe737d71472bd96ab550052a77a65f4110f8d9
VahuBali <67645126+VahuBali@users.noreply.github.com>
2021-07-29 21:07:16 -0400

    added a listen page

---
+++ src/components/Listen.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default function Listen() {
+
+    const rssfeed = 'https://feeds.redcircle.com/b95d4f68-9f91-4009-b9a8-9c2c0b31f68b'
+
+    return (
+        <div>
+
+        </div>
+    )
+}
